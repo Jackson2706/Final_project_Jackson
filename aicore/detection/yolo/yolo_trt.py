0e7c34812f35db0245eee9afcb50ca1d3cf91405
Jackson2706 <dung27062002@gmail.com>
2024-03-23 15:28:33 +0700

    implement Yolo algorithm, using framework TensorRT, abtract class

--- aicore/detection/yolo/yolo_trt.py
+++ aicore/detection/yolo/yolo_trt.py
@@ -1,27 +1,129 @@
 
 from abc import ABC
 import numpy as np
+import ctypes
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
 
 class YoloVehicleDetector(ABC):
-    def __init__(self, library, engine, input_height, input_width, categories, conf_thres, nms_thres):
+    def __init__(self, library, engine, categories, conf_thres, nms_thres, len_all_result, len_one_result):
+        """
+        Initializes the YOLO Vehicle Detector.
+
+        Parameters:
+        - library: Path to the library.
+        - engine: Path to the engine file.
+        - categories: List of categories.
+        - conf_thres: Confidence threshold.
+        - nms_thres: Non-maximum suppression threshold.
+        - len_all_result: Length of all results.
+        - len_one_result: Length of one result.
+        """
         self.library = library
         self.engine = engine
-        self.input_height = input_height
-        self.input_width = input_width
         self.categories = categories
         self.conf_thres = conf_thres
         self.nms_thres = nms_thres
+        self.LEN_ALL_RESULT = len_all_result
+        self.LEN_ONE_RESULT = len_one_result
+        self.host_inputs = []
+        self.cuda_inputs = []
+        self.host_outputs = []
+        self.cuda_outputs = []
+        self.bindings = []
 
+        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+
+        ctypes.CDLL(library)
+
+        with open(engine, 'rb') as f:
+            serialized_engine = f.read()
+
+        runtime = trt.Runtime(TRT_LOGGER)
+        self.engine = runtime.deserialize_cuda_engine(serialized_engine)
+        self.batch_size = self.engine.max_batch_size
+
+        for binding in self.engine:
+            size = trt.volume(self.engine.get_binding_shape(binding)) * self.batch_size
+            dtype = trt.nptype(self.engine.get_binding_dtype(binding))
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            cuda_mem = cuda.mem_alloc(host_mem.nbytes)
+
+            self.bindings.append(int(cuda_mem))
+            if self.engine.binding_is_input(binding):
+                self.input_w = self.engine.get_binding_shape(binding)[-1]
+                self.input_h = self.engine.get_binding_shape(binding)[-2]
+                self.host_inputs.append(host_mem)
+                self.cuda_inputs.append(cuda_mem)
+            else:
+                self.host_outputs.append(host_mem)
+                self.cuda_outputs.append(cuda_mem)
+
+    def check_gpu(self):
+        """
+        Checks if the code is running on GPU.
+
+        Returns:
+        - gpu_available: True if GPU is available, False otherwise.
+        """
+        gpu_available = cuda.Device.count() > 0
+        if not gpu_available:
+            print("No GPU device found. Running on CPU.")
+        return gpu_available
+    
     def preprocess_image(self, img):
+        """
+        Preprocesses an image.
+
+        Parameters:
+        - img: Input image.
+
+        Returns:
+        - preprocessed_image: Preprocessed image.
+        """
         pass
+
     def run_native_inference(self, image):
+        """
+        Runs native inference.
+
+        Parameters:
+        - image: Input image.
+
+        Returns:
+        - result: Inference result.
+        """
         pass
-    def postprocess(self, output):
+
+    def postprocess(self, output, origin_h, origin_w):
+        """
+        Postprocesses the output.
+
+        Parameters:
+        - output: Model output.
+        - origin_h: Original image height.
+        - origin_w: Original image width.
+
+        Returns:
+        - processed_output: Processed output.
+        """
         pass
     
     def _NMS(self, prediction, origin_h, origin_w):
+        """
+        Performs Non-Maximum Suppression (NMS).
+
+        Parameters:
+        - prediction: Model prediction.
+        - origin_h: Original image height.
+        - origin_w: Original image width.
+
+        Returns:
+        - boxes: Detected bounding boxes after NMS.
+        """
         boxes = prediction[prediction[:, 4] >= self.conf_thres]
-        boxes[:, :4] = self.xywh2xyxy(origin_h, origin_w, boxes[:, :4])
+        boxes[:, :4] = self._xywh2xyxy(origin_h, origin_w, boxes[:, :4])
         boxes[:, 0] = np.clip(boxes[:, 0], 0, origin_w -1)
         boxes[:, 2] = np.clip(boxes[:, 2], 0, origin_w -1)
         boxes[:, 1] = np.clip(boxes[:, 1], 0, origin_h -1)
@@ -30,7 +132,7 @@ class YoloVehicleDetector(ABC):
         boxes = boxes[np.argsort(-confs)]
         keep_boxes = []
         while boxes.shape[0]:
-            large_overlap = self.bbox_iou(np.expand_dims(boxes[0, :4], 0), boxes[:, :4]) > self.nms_thres
+            large_overlap = self._bbox_iou(np.expand_dims(boxes[0, :4], 0), boxes[:, :4]) > self.nms_thres
             label_match = boxes[0, -1] == boxes[:, -1]
             # Indices of boxes with lower confidence scores, large IOUs and matching labels
             invalid = large_overlap & label_match
@@ -40,6 +142,17 @@ class YoloVehicleDetector(ABC):
         return boxes
     
     def _bbox_iou(self, box1, box2, x1y1x2y2=True):
+        """
+        Calculates Intersection over Union (IoU) of two bounding boxes.
+
+        Parameters:
+        - box1: First bounding box.
+        - box2: Second bounding box.
+        - x1y1x2y2: Format of bounding box coordinates.
+
+        Returns:
+        - iou: Intersection over Union (IoU) value.
+        """
         if not x1y1x2y2:
             # Transform from center and width to exact coordinates
             b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
@@ -65,6 +178,17 @@ class YoloVehicleDetector(ABC):
         return iou
 
     def _xywh2xyxy(self, origin_h, origin_w, x):
+        """
+        Converts bounding box coordinates from (x, y, w, h) to (x1, y1, x2, y2) format.
+
+        Parameters:
+        - origin_h: Original image height.
+        - origin_w: Original image width.
+        - x: Bounding box coordinates.
+
+        Returns:
+        - y: Converted bounding box coordinates.
+        """
         y = np.zeros_like(x)
         r_w = self.input_w / origin_w
         r_h = self.input_h / origin_h
---
+++ aicore/detection/yolo/yolov5/yolov5_trt.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+import pycuda.driver as cuda
+from datetime import datetime
+
+from aicore.detection.yolo.yolo_trt import YoloVehicleDetector
+from aicore.component.trajectory import Trajectory
+
+
+class Yolov5_TRT(YoloVehicleDetector):
+    def __init__(self, library, engine, categories, conf_thres, nms_thres, len_all_result, len_one_result):
+        """
+        Initializes the YOLOv5 TensorRT object.
+
+        Parameters:
+        - library: Path to the library.
+        - engine: Path to the engine file.
+        - categories: List of categories.
+        - conf_thres: Confidence threshold.
+        - nms_thres: Non-maximum suppression threshold.
+        - len_all_result: Length of all results.
+        - len_one_result: Length of one result.
+        """
+        super().__init__(library, engine, categories, conf_thres, nms_thres, len_all_result, len_one_result)
+    
+    def preprocess_image(self, img):
+        """
+        Preprocesses an image for YOLOv5 TensorRT inference.
+
+        Parameters:
+        - img: Input image.
+
+        Returns:
+        - image: Preprocessed image.
+        - image_raw: Original image.
+        - h: Original image height.
+        - w: Original image width.
+        """
+        super().preprocess_image(img)
+        image_raw = img
+        h, w, c = image_raw.shape
+        image = cv2.cvtColor(image_raw, cv2.COLOR_BGR2RGB)
+        r_w = self.input_w / w
+        r_h = self.input_h / h
+        if r_h > r_w:
+            tw = self.input_w
+            th = int(r_w * h)
+            tx1 = tx2 = 0
+            ty1 = int((self.input_h - th) / 2)
+            ty2 = self.input_h - th - ty1
+        else:
+            tw = int(r_h * w)
+            th = self.input_h
+            tx1 = int((self.input_w - tw) / 2)
+            tx2 = self.input_w - tw - tx1
+            ty1 = ty2 = 0
+        image = cv2.resize(image, (tw, th))
+        image = cv2.copyMakeBorder(image, ty1, ty2, tx1, tx2, cv2.BORDER_CONSTANT, None, (128, 128, 128))
+        image = image.astype(np.float32)
+        image /= 255.0
+        image = np.transpose(image, [2, 0, 1])
+        image = np.expand_dims(image, axis=0)
+        image = np.ascontiguousarray(image)
+        return image, image_raw, h, w
+    
+    def postprocess(self, output, origin_h, origin_w):
+        """
+        Postprocesses the output of YOLOv5 TensorRT inference.
+
+        Parameters:
+        - output: Model output.
+        - origin_h: Original image height.
+        - origin_w: Original image width.
+
+        Returns:
+        - result_boxes: Detected bounding boxes.
+        - result_scores: Confidence scores of detected boxes.
+        - result_classid: Class IDs of detected boxes.
+        """
+        super().postprocess(output, origin_h, origin_w)
+        num = int(output[0])
+        pred = np.reshape(output[1:], (-1, self.LEN_ONE_RESULT))[:num, :]
+        pred = pred[:, :6]
+        boxes = self._NMS(pred, origin_h, origin_w)
+        result_boxes = boxes[:, :4] if len(boxes) else np.array([])
+        result_scores = boxes[:, 4] if len(boxes) else np.array([])
+        result_classid = boxes[:, 5] if len(boxes) else np.array([])
+        return result_boxes, result_scores, result_classid
+    
+    def run_native_inference(self, image):
+        """
+        Runs native inference for YOLOv5 TensorRT.
+
+        Parameters:
+        - image: Input image.
+
+        Returns:
+        - boxes: Detected bounding boxes.
+        """
+        super().run_native_inference(image)
+        input_image, image_raw, origin_h, origin_w = self.preprocess_image(img=image)
+        np.copyto(self.host_inputs[0], input_image.ravel())
+        stream = cuda.Stream()
+        self.context = self.engine.create_execution_context()
+        cuda.memcpy_htod_async(self.cuda_inputs[0], self.host_inputs[0], stream)
+        self.context.execute_async(self.batch_size, self.bindings, stream_handle=stream.handle)
+        cuda.memcpy_dtoh_async(self.host_outputs[0], self.cuda_outputs[0], stream)
+        stream.synchronize()
+        output = self.host_outputs[0]
+                
+        for i in range(self.batch_size):
+            result_boxes, result_scores, result_classid = self.postprocess(output[i * self.LEN_ALL_RESULT: (i + 1) * self.LEN_ALL_RESULT], origin_h, origin_w)
+            
+        boxes = []
+        for j in range(len(result_boxes)):
+            box = result_boxes[j]
+            lbl = self.categories[int(result_classid[j])]
+            conf = result_scores[j]
+            x1,y1,x2,y2 = box
+            x_center = (x1+x2)/2
+            y_center = (y1+y2)/2
+            w = x2 - x1
+            h = y2 - y1
+            boxes.append(Trajectory(x_center=x_center, y_center=y_center, width=w, height=h, label=lbl, conf=conf, time_stamp=datetime.now()))
+        return boxes
