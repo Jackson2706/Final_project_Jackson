03cf12a794211278ea34c4510dce776dce3a6d4e
Jackson2706 <dung27062002@gmail.com>
2024-05-04 13:11:48 +0700

    Test pipeline

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from application.streaming.streaming import IPCameraFrameRecoder
 from config.constant import CAMERA_URL, MAX_FRAME_QUEUE_SIZE, TENSORRT_LIB_PATH, TENSORRT_MODEL_ENGINE, CATEGORY, LEN_ALL_RESULT, LEN_ONE_RESULT, IOU_THRESHOLD, CONF_THRESH, ONNX_MODEL_PATH, INPUT_SHAPE, DEEPSORT_CONFIG_PATH
 from aicore.detection.yolo_onnx.yolov5.yolov5_onnx import Yolov5_Onnx
 from aicore.tracking.deep_sort.tracker import DeepSortInference, draw_bboxes
+## Test
+from aicore.logic_shape_filter.filter.LineFilter import LineFilter
+from aicore.component.Line import Line
+from aicore.component.Point import Point
+
+line_location = Line(Point(45, 411), Point(551,455))
+linefilter = LineFilter("Test_line", 1, line_location, 0, 0, ["car"], -1)
+####
 
 cap = IPCameraFrameRecoder(camera_url=CAMERA_URL,
                            max_queue_size=MAX_FRAME_QUEUE_SIZE,
@@ -22,7 +30,8 @@ for frame in cap.read_frame():
     frame = cv2.resize(frame, INPUT_SHAPE)
     frame = draw_bboxes(image=frame, bboxes=result)
     cv2.imshow("test", frame)
-
+    filter_result = linefilter.make_filtering(result)
+    print(len(filter_result))
     if cv2.waitKey(1) == ord("q"):
         break
 
