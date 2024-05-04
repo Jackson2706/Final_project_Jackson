a1c0347badbcd0740c907a767ba0b0690298e1da
Jackson2706 <dung27062002@gmail.com>
2024-05-04 13:10:59 +0700

    update Abstract class logic_shape_filter

--- aicore/logic_shape_filter/logic_shape_filter.py
+++ aicore/logic_shape_filter/logic_shape_filter.py
@@ -1,10 +1,18 @@
 from abc import ABC
+from typing import Union, Dict, Any
 
+from aicore.component.DetectedObject import DetectedObject
+from aicore.component.Line import Line
+from aicore.component.Rect import Rect
 class LogicShapeFilter(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, id: int, location: Union[Line, Rect], created_at: int, updated_at: int):
+        self.name = name
+        self.id = id
+        self.location = location
+        self.created = created_at
+        self.updated = updated_at
 
-    def make_filtering(self):
+    def make_filtering(self, source_filter: Dict[Any, DetectedObject]):
         pass
 
     def equals(self):
