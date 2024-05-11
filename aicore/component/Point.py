b5adebb3fbe1348fa3b4746d44c8ec1fc6c015f2
Jackson2706 <dung27062002@gmail.com>
2024-05-11 17:24:15 +0700

    add function getter in Rect and Point class

--- aicore/component/Point.py
+++ aicore/component/Point.py
@@ -1,7 +1,10 @@
 class Point:
     def __init__(self, X, Y):
-        self.X = X
-        self.Y = Y
+        self._X = X
+        self._Y = Y
+
+    def get_point(self):
+        return self._X, self._Y
 
     def __str__(self):
-        return f"Point: ({self.X}, {self.Y}"
+        return f"Point: ({self._X}, {self._Y}"
--- aicore/component/Rect.py
+++ aicore/component/Rect.py
@@ -1,8 +1,16 @@
 from aicore.component.Point import Point
+
+
 class Rect:
     def __init__(self, top_left_point: Point, bottom_right_point: Point):
-        self.top_left_point = top_left_point
-        self.bottom_right_point = bottom_right_point
+        self._top_left_point = top_left_point
+        self._bottom_right_point = bottom_right_point
+
+    def get_top_left_point(self):
+        return self._top_left_point
+
+    def get_bottom_right_point(self):
+        return self._bottom_right_point
 
     def __str__(self):
-        return f"Rect: Top Left {self.top_left_point} - Bottom Right {self.bottom_right_point}"
+        return f"Rect: Top Left {self._top_left_point} - Bottom Right {self._bottom_right_point}"
